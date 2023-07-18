03f2f277a3dc1cc0a950c4acaa7c57cdcec934b2
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-18 22:27:11 +0900

    🐛 : Fix Styled-components Bug not applicable

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,32 @@
-import Header from "@/components/common/header/Header";
-import { Html, Head, Main, NextScript } from "next/document";
-import React from "react";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-const _document = () => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Header />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-export default _document;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initalialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initalialProps,
+        styles: (
+          <>
+            {initalialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
