4bfc22ad1bf28940a2b3a7d39746ecb663bc1338
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-19 22:00:01 +0900

    🎨 : Add Header Link

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import * as S from "./Header.style";
+import Link from "next/link";
 
 const Header = () => {
   const [isSignIn, setIsSignIn] = useState<boolean>(false);
   return (
     <S.MainHeader>
-      <S.Logo>Logo</S.Logo>
+      <S.Logo href={"/"}>Logo</S.Logo>
       <div>
         {isSignIn ? (
           <p>검색</p>
         ) : (
-          <>
-            <p>SignIn</p>
-            <p>SignUp</p>
-          </>
+          <S.CotentsWrap>
+            <S.ContentsContext href={"/auth/signin"}>SignIn</S.ContentsContext>
+            <S.ContentsContext href={"/auth/signup"}>SignUp</S.ContentsContext>
+          </S.CotentsWrap>
         )}
       </div>
     </S.MainHeader>
