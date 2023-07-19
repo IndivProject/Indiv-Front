dc251da77e2438bb8e86c0b21bb58b49f1fce03a
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-19 22:13:11 +0900

    :lipstick: : Add Auth Form

---
+++ src/styles/Auth.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MainAuthForm = styled.div`
+  margin: auto;
+  margin-top: 5rem;
+  width: 50%;
+  height: 40rem;
+  padding: 1rem;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+`;
