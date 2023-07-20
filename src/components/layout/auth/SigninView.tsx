53712ec2aec704ec263392a0af3283de9b424b34
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-20 20:50:42 +0900

    :art: : Add Auth Input Components

--- src/components/layout/auth/SigninView.tsx
+++ src/components/layout/auth/SigninView.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import AuthInput from "@/components/common/auth/AuthInput";
+import React, { ChangeEvent, useState } from "react";
+
+type UserKey = "userId" | "userPassword";
+
+type RecordUser = Record<UserKey, string>;
 
 const SigninView = () => {
+  const [userInfo, setUserInfo] = useState<RecordUser>({
+    userId: "",
+    userPassword: "",
+  });
+
+  const ChangeUserIdHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserInfo({ ...userInfo, userId: e.target.value });
+  };
+
+  const ChangeUserPasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserInfo({ ...userInfo, userPassword: e.target.value });
+  };
+
   return (
     <>
-      <p></p>
-      <div>SigninView</div>
+      <AuthInput
+        title="아아디"
+        type="text"
+        placeholder="아이디를 입력해주세요."
+        value={userInfo.userId}
+        changeValueHandler={ChangeUserIdHandler}
+      />
+      <AuthInput
+        title="비밀번호"
+        type="password"
+        placeholder="비밀번호를 입력해주세요."
+        value={userInfo.userPassword}
+        changeValueHandler={ChangeUserPasswordHandler}
+      />
     </>
   );
 };
