9fb61350ec9f99919e445b1e43695349b7c436b4
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-25 14:47:03 +0900

    FIX :: Search Bar Style

--- src/components/common/search/style.ts
+++ src/components/common/search/style.ts
@@ -5,9 +5,9 @@ export const MainLayout = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 50%;
+  width: 100%;
   height: 1rem;
-  margin: 0rem auto;
+  margin: 2rem auto;
   padding: 1rem;
   background-color: #e9e9e9;
   border-radius: 50rem;
