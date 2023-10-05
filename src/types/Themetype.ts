1c4d4b188c12bf353a6d47b0f408b036585dc865
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-05 13:47:15 +0900

    Add Theme Type

---
+++ src/types/Themetype.ts
@@ -0,0 +1,8 @@
+export interface ThemeType {
+  PersonalGreen100: "#A8F8E9";
+  PersonalGreen200: "#8EDACC";
+  PersonalGreen300: "#84BDB3";
+  Gray100: "#DEDEDE";
+  Gray300: "#B0B0B0";
+  Gray500: "#8E8E8E";
+}
