5f63d9a7c979129347027ba8c60c850557ce01c2
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-12 19:57:55 +0900

    Add : Signup Chapter

---
+++ src/components/common/auth/SignupChapter/Chapter3/Chapter3.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chapter3 = () => {
+  return <div>Chapter3</div>;
+};
+
+export default Chapter3;
---
+++ src/components/common/auth/SignupChapter/Chapter4/Chapter4.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chapter4 = () => {
+  return <div>Chapter4</div>;
+};
+
+export default Chapter4;
