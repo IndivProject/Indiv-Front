b2cbefcbdc657c8b906dc4b63859f36cb5b444c3
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-12 20:19:10 +0900

    fix : chapter

--- src/components/layout/auth/SignupView.tsx
+++ src/components/layout/auth/SignupView.tsx
@@ -1,67 +1,31 @@
-import AuthInput from "@/components/common/auth/AuthInput";
-import React, { ChangeEvent, useState } from "react";
-import IndivLogo from "@/../public/asset/IndivLogo.svg";
-import Image from "next/image";
-
-type NewUserKey = "userId" | "userPassword" | "userPasswordCheck" | "userName";
-
-type NewRecordUser = Record<NewUserKey, string>;
+import Chapter1 from "@/components/SignupChapter/Chapter1";
+import Chapter2 from "@/components/SignupChapter/Chapter2";
+import Chapter3 from "@/components/SignupChapter/Chapter3";
+import Chapter4 from "@/components/SignupChapter/Chapter4";
+import React, { useState } from "react";
 
 const SignupView = ({ children }: { children: React.ReactElement }) => {
-  const [newUserInfo, setNewUserInfo] = useState<NewRecordUser>({
-    userId: "",
-    userPassword: "",
-    userPasswordCheck: "",
-    userName: "",
-  });
+  const [pageIdx, setPageIdx] = useState<number>(0);
 
-  const ChangeNewUserIdHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewUserInfo({ ...newUserInfo, userId: e.target.value });
-  };
-  const ChangeNewUserPasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewUserInfo({ ...newUserInfo, userPassword: e.target.value });
-  };
-  const ChangeNewUserPasswordCheckHandler = (
-    e: ChangeEvent<HTMLInputElement>
-  ) => {
-    setNewUserInfo({ ...newUserInfo, userPasswordCheck: e.target.value });
-  };
-  const ChangeNewUserNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewUserInfo({ ...newUserInfo, userName: e.target.value });
+  const setChapter = () => {
+    switch (pageIdx) {
+      case 0:
+        return <Chapter1 />;
+      case 1:
+        return <Chapter2 />;
+      case 2:
+        return <Chapter3 />;
+      case 3:
+        return <Chapter4 />;
+      default:
+        return null;
+    }
   };
 
   return (
     <>
-      <Image src={IndivLogo} alt="" />
       {children}
-      <AuthInput
-        title="아이디"
-        type="text"
-        placeholder="새 아이디를 입력해주세요."
-        value={newUserInfo.userId}
-        changeValueHandler={ChangeNewUserIdHandler}
-      />
-      <AuthInput
-        title="비밀번호"
-        type="password"
-        placeholder="새 비밀번호를 입력해주세요."
-        value={newUserInfo.userPassword}
-        changeValueHandler={ChangeNewUserPasswordHandler}
-      />
-      <AuthInput
-        title="비밀번호 확인"
-        type="password"
-        placeholder="비밀번호를 한번 더 입력해주세요."
-        value={newUserInfo.userPasswordCheck}
-        changeValueHandler={ChangeNewUserPasswordCheckHandler}
-      />
-      <AuthInput
-        title="이름"
-        type="text"
-        placeholder="사용하실 이름을 입력해주세요."
-        value={newUserInfo.userName}
-        changeValueHandler={ChangeNewUserNameHandler}
-      />
+      {setChapter()}
     </>
   );
 };
