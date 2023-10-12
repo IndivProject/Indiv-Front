9071592b381f6288f1dffd6343dac75977ca9b4e
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-12 15:35:22 +0900

    fix : theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import Header from "@/components/common/header/Header";
 import "@/styles/globals.css";
+import { theme } from "@/styles/theme";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-
-const theme = {
-  PersonalGreen100: "#A8F8E9",
-  PersonalGreen200: "#8EDACC",
-  PersonalGreen300: "#84BDB3",
-  Gray100: "#DEDEDE",
-  Gray300: "#B0B0B0",
-  Gray500: "#8E8E8E",
-};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  PersonalGreen100: "#A8F8E9",
+  PersonalGreen200: "#8EDACC",
+  PersonalGreen300: "#76CBBC",
+  PersonalGreen500: "#84BDB3",
+  Gray100: "#DEDEDE",
+  Gray300: "#B0B0B0",
+  Gray500: "#8E8E8E",
+};
