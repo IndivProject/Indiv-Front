8e475047896d252aa6497f90baf7dccf55f52e78
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-13 12:30:06 +0900

    Add : Logo

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
-import IndivLogo from "@/asset/IndivLogo.svg";
 import * as S from "./Header.style";
+import IndivLogo from "@/../public/asset/IndivLogo.svg";
+import Image from "next/image";
 
 const Header = () => {
   const [isSignIn, setIsSignIn] = useState<boolean>(false);
+
   return (
     <S.MainHeader>
-      <S.Logo href={"/"}>Logo</S.Logo>
+      <S.Logo href={"/"}>
+        <Image
+          src={IndivLogo}
+          alt=""
+          style={{
+            width: "100px",
+          }}
+        />
+      </S.Logo>
       <div>
         {isSignIn ? (
           <p>검색</p>
         ) : (
           <S.CotentsWrap>
-            <S.ContentsContext href={"/auth/signin"}>SignIn</S.ContentsContext>
-            <S.ContentsContext href={"/auth/signup"}>SignUp</S.ContentsContext>
+            <S.ContentsContext href={"/auth/signin"}>로그인</S.ContentsContext>
+            <S.ContentsContext href={"/auth/signup"}>
+              회원가입
+            </S.ContentsContext>
           </S.CotentsWrap>
         )}
       </div>
