052a9a26e5c04f66f3dbb00fcce14a0a753df910
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-13 12:29:57 +0900

    Add : Template

--- src/components/layout/main/MainView.tsx
+++ src/components/layout/main/MainView.tsx
@@ -2,9 +2,15 @@ import Post from "@/components/common/post/Post";
 import Search from "@/components/common/search/Search";
 import * as S from "./style";
 import React from "react";
+import Main from "@/components/Main/Main";
+import DefaultTemplate from "@/components/common/Template/DefaultTemplate/DefaultTemplate";
 
 const MainView = () => {
-  return <></>;
+  return (
+    <DefaultTemplate>
+      <Main />
+    </DefaultTemplate>
+  );
 };
 
 export default MainView;
